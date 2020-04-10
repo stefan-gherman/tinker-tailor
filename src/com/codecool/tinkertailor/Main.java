18a7c9b2b9c0e0c9daf5f5bd907b37c4939649b9
Stefan Gherman <scgheman@gmail.com>
2020-04-10 19:09:48 +0300

    Finish assignment

--- src/com/codecool/tinkertailor/Main.java
+++ src/com/codecool/tinkertailor/Main.java
@@ -8,5 +8,14 @@ public class Main {
 	    TinkerTailor tinkerTailor = new TinkerTailor(5, 3);
 	    List outcome = tinkerTailor.execute();
 	    System.out.println("The outcome of the game is: " + outcome);
+
+		TinkerTailor tinkerTailor2 = new TinkerTailor(6, 5);
+		List outcome2 = tinkerTailor2.execute();
+		System.out.println("The outcome of the game is: " + outcome2);
+
+
+		TinkerTailor tinkerTailor3 = new TinkerTailor(3, 2);
+		List outcome3 = tinkerTailor3.execute();
+		System.out.println("The outcome of the game is: " + outcome3);
     }
 }
--- src/com/codecool/tinkertailor/TinkerTailor.java
+++ src/com/codecool/tinkertailor/TinkerTailor.java
@@ -13,17 +13,60 @@ class TinkerTailor {
     }
 
     public List execute() {
-        List<Integer> outcome = new ArrayList<>();
+        List<Integer> results = new ArrayList<>();
+        List<Integer> initialList = new ArrayList<>();
 
-        outcome.add(3);
-        outcome.add(1);
-        outcome.add(5);
-        outcome.add(2);
-        outcome.add(4);
+
+        for(int i=0; i<n; i++) {
+            initialList.add(i+1);
+        }
+        int steps = initialList.size() -1;
+
+        int indexStartNext = 0;
+
+
 
         // Works for n = 5 and k = 3
         // Well, you should come up with a more general algorithm :)
-
-        return outcome;
+        while ( steps > 0) {
+            if(initialList.size() == 2) {
+                if(k % 2 == 0) {
+                    if(indexStartNext == 0) {
+                        results.add(initialList.get(1));
+                        initialList.remove(1);
+                    } else {
+                        results.add(initialList.get(0));
+                        initialList.remove(0);
+                    }
+                } else {
+                    if( indexStartNext == 0 ){
+                        results.add(initialList.get(0));
+                        initialList.remove(0);
+                    } else {
+                        results.add(initialList.get(1));
+                        initialList.remove(1);
+                    }
+                }
+            } else {
+                if(indexStartNext + (k-1) < initialList.size()){
+                    results.add(initialList.get(indexStartNext + (k-1)));
+                    initialList.remove(indexStartNext + (k-1));
+                    indexStartNext = indexStartNext + (k-1);
+                    if(indexStartNext >= initialList.size()) {
+                        indexStartNext = 0;
+                    }
+                } else {
+                    results.add(initialList.get((indexStartNext + (k-1)) - initialList.size()));
+                    initialList.remove(indexStartNext + (k-1) - initialList.size());
+                    indexStartNext = (indexStartNext + (k-1)) - (initialList.size()+1);
+                    if(indexStartNext >= initialList.size()) {
+                        indexStartNext = 0;
+                    }
+                }
+            }
+            steps-=1;
+        }
+        results.add(initialList.get(0));
+        return results;
     }
 }
